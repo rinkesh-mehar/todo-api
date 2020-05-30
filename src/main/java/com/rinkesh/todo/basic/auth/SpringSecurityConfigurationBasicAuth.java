c7c985d2eda25c113b9b4b8bf3dd1b487f7447fc
Rinkesh Mehar <rinkesh.k.mehar@gmail.com>
2020-05-30 10:09:14 +0530

    #76: Implemented basic http authorization for spring security

---
+++ src/main/java/com/rinkesh/todo/basic/auth/SpringSecurityConfigurationBasicAuth.java
@@ -0,0 +1,27 @@
+package com.rinkesh.todo.basic.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfigurationBasicAuth extends WebSecurityConfigurerAdapter
+{
+	@Override
+	protected void configure(HttpSecurity http) throws Exception
+	{
+		http
+				.csrf().disable()// this is force post and put request. disabling for option in response
+				.authorizeRequests()
+				.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()// this use to permit all the option request and block the other
+				.anyRequest()
+				.authenticated()
+				.and()
+				// .formLogin()
+				// .and()
+				.httpBasic(); // for other request we allowed to use basic http authentication
+	}
+}
